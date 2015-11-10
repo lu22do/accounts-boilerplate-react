b5ac5a62d205dcb29216e319975cfcc9fb259422
lu22do <lpierre_local@MacBook.local>
2015-11-09 22:32:36 -0800

    enabled topbar focus

--- client/controllers/topbar.js
+++ client/controllers/topbar.js
@@ -1,3 +1,15 @@
+Template.topbar.helpers({
+  stuffsactive: function() {    
+    return Router.current().route.getName() === 'stuffs' ? 'active' : '';
+  }, 
+  newstuffactive: function() {    
+    return Router.current().route.getName() === 'newstuff' ? 'active' : '';
+  },  
+  usersactive: function() {    
+    return Router.current().route.getName() === 'users' ? 'active' : '';
+  }  
+});
+
 Template.topbar.events({
   'click #logout': function() {
     Meteor.logout(function() {
